7974195d2c72cc6e00142a09d881b771f564ad5c
Damian <dhfxblog@gmail.com>
2020-04-01 18:54:03 +0200

    Updates

---
+++ precache-manifest.6fda0ff6e088c74493e6d2b2aaad1467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe054b1b267b2df66bb0",
+    "url": "/haydulski/ActualWeather/static/css/main.d8e19001.chunk.css"
+  },
+  {
+    "revision": "fe054b1b267b2df66bb0",
+    "url": "/haydulski/ActualWeather/static/js/main.3f68c156.chunk.js"
+  },
+  {
+    "revision": "c860dfc1947d0bd677b0",
+    "url": "/haydulski/ActualWeather/static/js/runtime~main.637ba654.js"
+  },
+  {
+    "revision": "f6d2ab47bb337dc705a8",
+    "url": "/haydulski/ActualWeather/static/js/2.b67fbdca.chunk.js"
+  },
+  {
+    "revision": "f97b8b4bc6bf3f599468daff26beb6b9",
+    "url": "/haydulski/ActualWeather/index.html"
+  }
+];
